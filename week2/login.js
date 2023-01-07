75f6a98e610a983422c256480ac9c0979b308791
Bellie1110 <bellie1110@gmail.com>
2023-01-07 22:46:34 +0800

    update

--- week2/login.js
+++ week2/login.js
@@ -1,13 +1,12 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 import axios from "https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.2/esm/axios.min.js";
 
-const url = "https://vue3-course-api.hexschool.io/v2"; // 請加入站點
-const path = "testadd";
+const url = "https://vue3-course-api.hexschool.io/v2";
+const path = "bella";
 const token = document.cookie.replace(
   /(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/,
   "$1"
 );
-console.log(token);
 
 axios.defaults.headers.common["Authorization"] = token;
 
@@ -24,14 +23,14 @@ createApp({
     login() {
       axios
         .post(`${url}/admin/signin`, this.user)
-        .then((res) => {
-          const { token, expired } = res.data;
+        .then((response) => {
+          const { token, expired } = response.data;
           document.cookie = `myToken=${token}; expires= ${new Date(expired)}`;
           window.location = "./products.html";
         })
-        // error
         .catch((error) => {
-          console.dir(error);
+          alert(error.response.data.message);
+          // console.dir(error.this.user);
         });
     },
   },
---
+++ week2/products.js
@@ -0,0 +1,51 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import axios from "https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.2/esm/axios.min.js";
+
+const url = "https://vue3-course-api.hexschool.io/v2"; // 請加入站點
+const path = "bella";
+
+createApp({
+  data() {
+    return {
+      products: [],
+      tempProduct: {},
+    };
+  },
+  methods: {
+    checkAdmin() {
+      axios
+        .post(`${url}/api/user/check`)
+        .then(() => {
+          this.getAllProducts();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          window.location = "login.html";
+        });
+    },
+    getAllProducts() {
+      axios
+        .get(`${url}/api/${path}/admin/products`)
+        .then((response) => {
+          this.products = response.data.products;
+        })
+        .catch((err) => {
+          console.dir(err);
+        });
+    },
+    openProduct(item) {
+      console.log(item);
+      this.tempProduct = item;
+    },
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)myToken\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin();
+  },
+}).mount("#app");
